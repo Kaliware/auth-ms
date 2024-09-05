87216f8bc9e0d92641479d9dae75cbc00eeaecf9
Damian Lourenço <noah.damian.tech@gmail.com>
2024-09-05 14:18:14 -0300

    refactor: remove unused dependencies
    feat(config): implement UTC timezone configuration

--- src/main/java/br/com/kaliware/ms/auth_ms/config/mapper/MapperConfiguration.java
+++
@@ -1,8 +0,0 @@
-package br.com.kaliware.ms.auth_ms.config.mapper;
-
-import org.mapstruct.MapperConfig;
-import org.mapstruct.ReportingPolicy;
-
-@MapperConfig(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
-public interface MapperConfiguration {
-}
